5f5837263090499ffef0ae228d4f48997c801e77
Joary13 <jolea13008@gmail.com>
2023-11-14 15:18:00 +0100

    localStorage, display products, and formatPrice done

--- src/fetchProducts.js
+++ src/fetchProducts.js
@@ -1,5 +1,13 @@
 import { allProductsUrl } from './utils.js';
 
-const fetchProducts = async () => {};
+const fetchProducts = async () => {
+  try {
+    const response = await fetch(allProductsUrl);
+    if (!response.ok) throw new Error("pb avec l'URL");
+    return response.json();
+  } catch (err) {
+    console.error(`voici la raison de l'erreur: ${err}`);
+  }
+};
 
 export default fetchProducts;
