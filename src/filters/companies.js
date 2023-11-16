d144741cb61607dd2349a55adf166d51b94c58d3
Joary13 <jolea13008@gmail.com>
2023-11-16 14:12:59 +0100

    sort by company finished without matching whith search input on purpose

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -2,12 +2,23 @@ import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
 
 const setupCompanies = (shop) => {
-  const compagny = getElement('.compagnies');
-  let compagnyList = new Set(shop.map((el) => el.company));
-  compagnyList = ['all', ...Array.from(compagnyList).sort()];
-  compagny.innerHTML = compagnyList
+  const company = getElement('.compagnies');
+  let companyList = new Set(shop.map((el) => el.company));
+  companyList = ['all', ...Array.from(companyList).sort()];
+  company.innerHTML = companyList
     .map((comp) => `<button class="company-btn">${comp}</button>`)
     .join('');
+  company.addEventListener('click', (e) => {
+    const companyTarget = e.target;
+    if (companyTarget.classList.contains('company-btn')) {
+      const companyName = companyTarget.textContent;
+      if (companyName === 'all') {
+        return display(shop, getElement('.products-container'));
+      }
+      const products = shop.filter((art) => art.company === companyName);
+      display(products, getElement('.products-container'));
+    }
+  });
 };
 
 export default setupCompanies;
