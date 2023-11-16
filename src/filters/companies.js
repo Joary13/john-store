a3be614de1f1aa74c648a3f7b0917ec35b6a4ce0
Joary13 <jolea13008@gmail.com>
2023-11-16 11:07:38 +0100

    list compagny displayed dynamically

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -1,6 +1,13 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
 
-const setupCompanies = () => {};
+const setupCompanies = (shop) => {
+  const compagny = getElement('.compagnies');
+  let compagnyList = new Set(shop.map((el) => el.company));
+  compagnyList = ['all', ...Array.from(compagnyList).sort()];
+  compagny.innerHTML = compagnyList
+    .map((comp) => `<button class="company-btn">${comp}</button>`)
+    .join('');
+};
 
 export default setupCompanies;
