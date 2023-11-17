6f25d01d5bce140a15a2171bd9d71300c4d93a39
Joary13 <jolea13008@gmail.com>
2023-11-17 11:43:38 +0100

    single product page done

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -9,6 +9,7 @@ import { openCart } from './toggleCart.js';
 import { findProduct } from '../store.js';
 import addToCartDOM from './addToCartDOM.js';
 // set items
+const cartItemCountDown = getElement('.cart-item-count');
 
 export const addToCart = (id) => {
   console.log(id);
--- src/filters/price.js
+++ src/filters/price.js
@@ -1,6 +1,31 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
 
-const setupPrice = () => {};
+const setupPrice = (store) => {
+  const priceValue = getElement('.price-value');
+  const priceLine = getElement('.price-filter');
+  // setup filter
+  const priceTab = store.map((product) => product.price);
+  let maxPrice = Math.max(...priceTab);
+  maxPrice = Math.ceil(maxPrice / 100);
+  priceLine.max = maxPrice;
+  priceLine.value = maxPrice;
+  priceLine.min = 0;
+  priceValue.textContent = `Value : $${maxPrice}`;
+
+  priceLine.addEventListener('input', () => {
+    const maxAmount = parseInt(priceLine.value);
+    priceValue.textContent = `Value : $${maxAmount}`;
+    const products = store.filter(
+      (product) => product.price / 100 <= maxAmount
+    );
+    if (products.length < 1) {
+      getElement('.products-container').innerHTML =
+        '<h3 class="filter-error">sorry, no products matched your search</h3>';
+      return;
+    }
+    display(products, getElement('.products-container'));
+  });
+};
 
 export default setupPrice;
